b3f119ca767a3bd7fcd883b186e39cc24d3bbfe0
GiannaLowry <glowry1@charlotte.edu>
2026-09-17 23:52:13 -0400

    refactor: move BankAccount class to separate module

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-class BankAccount:
-
-    bank_name = "The Bank" # class attribute
-
-    def __init__(self, customer_name, current_balance, minimum_balance):
-        self.customer_name = customer_name
-        self.current_balance = current_balance
-        self.minimum_balance = minimum_balance
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.current_balance += amount
-        else:
-            print("Amount must be greater than 0.")
-
-    def withdraw(self, amount):
-        if amount <= 0:
-            print("Amount must be greater than 0.")
-        elif amount > self.current_balance:
-            print("Insufficient balance.")
-        elif self.current_balance - amount < self.minimum_balance:
-            print("Withdrawal will put account under the minimum.")
-        else:
-            self.current_balance -= amount
-
-    def print_customer_information(self):
-        print(f"\nBank name: {self.bank_name}")
-        print(f"Customer Name: {self.customer_name}")
-        print(f"Current balance: {self.current_balance:.2f} ")
-        print(f"Minimum balance: {self.minimum_balance:.2f}\n")
-
 test1 = BankAccount("Alexa", 300, 100)
 test1.deposit(600)
 test1.withdraw(80)
