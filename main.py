ac127eb375031ea7494193bd2dbcf58bc0a618eb
GiannaLowry <glowry1@charlotte.edu>
2026-09-18 14:37:25 -0400

    test: add SavingAccount test instances

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-test1 = BankAccount("Alexa", 300, 100)
-test1.deposit(600)
-test1.withdraw(80)
-test1.print_customer_information()
-test2 = BankAccount("Bob", 5000, 100)
-test2.deposit(800)
-test2.withdraw(5701)
-test2.print_customer_information()
-test3 = BankAccount("David", 170, 100)
-test3.withdraw(500)
+from BankAccount import BankAccount
+from SavingsAccount import SavingsAccount
+
+# first test instance for SavingsAccount subclass
+savings_test1 = SavingsAccount("Alexa", 700, 100, "07654", "1234567", 0.042)
+savings_test1.deposit(2000)
+savings_test1.add_interest()
+savings_test1.get_interest_rate()
+savings_test1.withdraw(55)
+savings_test1.print_customer_information()
+
+# second test instance for Savings Account subclass
+savings_test2 = SavingsAccount("David", 400, 50, "9876545", "1234567", 0.032)
+savings_test2.withdraw(500)
+savings_test2.deposit(205)
+savings_test2.add_interest()
+savings_test2.get_interest_rate()
+savings_test2.print_customer_information()
