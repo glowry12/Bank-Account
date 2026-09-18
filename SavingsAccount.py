5029a5a4694751f5754a012f735d87ae84d86fbd
GiannaLowry <glowry1@charlotte.edu>
2026-09-18 14:06:53 -0400

    feat: add SavingsAccount subclass

---
+++ SavingsAccount.py
@@ -0,0 +1,12 @@
+class SavingsAccount(BankAccount):
+
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, interest_rate):
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
+        self.interest_rate = interest_rate
+
+    def get_interest_rate(self):
+        return self.interest_rate
+
+    def add_interest (self):
+        interest = self.interest_rate * self.current_balance
+        self.current_balance += interest
